4b60e8143dc3f6ecc4710d2809359075a2c1c846
curder <curder@foxmail.com>
2018-04-16 22:26:25 +0800

    添加路由文件

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group([ 'middleware' => 'pjax' ], function() {
+    Route::view('/', 'welcome');
+    Auth::routes();    
+    Route::get('/home', 'HomeController@index')->name('home');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
